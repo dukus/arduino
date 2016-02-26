48da2157cc143e0cf5bac4343d010af67493144b
Duka Istvan <dukusi@gmail.com>
2016-02-26 12:10:52 +0200

    Bluetooth, lcd, sd card

--- DataLogger/testLcd/testLcd.ino
+++ DataLogger/testLcd/testLcd.ino
@@ -1,6 +1,9 @@
+
 //YWROBOT
 //Compatible with the Arduino IDE 1.0
 //Library version:1.1
+#include <SD.h>
+#include <SPI.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DS3232RTC.h>        //http://github.com/JChristensen/DS3232RTC
@@ -11,37 +14,72 @@
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 Adafruit_BMP085 bmp;
-
+File myFile;
 //----------------------
 String time = "";
 int internTemp = 0;
-float temp=0;
+float temp = 0;
 float input_voltage = 0.0;
 float baterry_voltage = 0.0;
-float r1=6830.0;
-float r2=2180.0;
+float r1 = 6830.0;
+float r2 = 2180.0;
+String inputString = "";
+unsigned long lcd_time;
 //----------------------
 void setup()
 {
   lcd.init();                      // initialize the lcd
 
   setSyncProvider(RTC.get);
-//  if (!bmp.begin()) {
-//  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  //  if (!bmp.begin()) {
+  //  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   bmp.begin();
-
+  // bluetooth
+  Serial.begin(9600);
+  Serial3.begin(9600);
+  inputString.reserve(200);
   // Print a message to the LCD.
-  lcd.backlight();
-  /*
-  lcd.setCursor(3, 0);
-  lcd.print("Hello, world!");
-  lcd.setCursor(2, 1);
-  lcd.print("Ywrobot Arduino!");
-  lcd.setCursor(0, 2);
-  lcd.print("Arduino LCM IIC 2004");
-  lcd.setCursor(2, 3);
-  lcd.print("Power By Ec-yuan!");
-  */
+  activateLcd();
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(53)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+ // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+  
 }
 
 
@@ -59,33 +97,63 @@ void loop()
       internTemp = RTC.temperature() / 4.;
     }
   }
+
+  if (lcd_time!=0 && millis() - lcd_time > 5 * 1000)
+  {
+    deActivateLcd();
+    lcd_time = 0;
+  }
+
+  if (Serial3.available()) {
+    activateLcd();
+    int inByte = Serial3.read();
+    Serial.write(inByte);
+  }
+
+  // read from port 0, send to port 1:
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial3.write(inByte);
+  }
 }
 
 void mesureVolts()
 {
   int analog_value = analogRead(A2);
-   temp = (analog_value * 5.0) / 1024.0; 
-   input_voltage = temp / (r2/(r1+r2));
-   analog_value = analogRead(A3);
-   temp = (analog_value * 5.0) / 1024.0; 
-   baterry_voltage = temp / (r2/(r1+r2));   
+  temp = (analog_value * 5.0) / 1024.0;
+  input_voltage = temp / (r2 / (r1 + r2));
+  analog_value = analogRead(A3);
+  temp = (analog_value * 5.0) / 1024.0;
+  baterry_voltage = temp / (r2 / (r1 + r2));
 }
 
 void mainDisplay(time_t t)
 {
 
   lcd.setCursor(0, 0);
-  lcd.print("Temp ="+ printI00(internTemp,' ')+"C");
+  lcd.print("Temp =" + printI00(internTemp, ' ') + "C");
   lcd.setCursor(15, 0);
   lcd.print(getTime(t));
   lcd.setCursor(0, 1);
-  lcd.print("Pres =" + printI00(bmp.readPressure()*0.00750061561303,' ')+"mmHg");
+  lcd.print("Pres =" + printI00(bmp.readPressure() * 0.00750061561303, ' ') + "mmHg");
   lcd.setCursor(0, 2);
   char buffer[10];
   String tem = dtostrf(input_voltage, 4, 1, buffer);
   String tem1 = dtostrf(baterry_voltage, 4, 1, buffer);
-  lcd.print("Volts=" +tem+"V/"+tem1+"V");
+  lcd.print("Volts=" + tem + "V/" + tem1 + "V");
 }
+
+void activateLcd()
+{
+  lcd_time = millis();
+  lcd.backlight();
+}
+
+void deActivateLcd()
+{
+  lcd.noBacklight();
+}
+
 
 
 //print date and time to Serial
@@ -130,4 +198,5 @@ String printI00(int val, char delim)
   if (delim > 0) res += delim;
   return res;
 }
+
 
