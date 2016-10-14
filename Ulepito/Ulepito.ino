51cd9be5ea115103d43ae17e62c017769d249f5b
Duka Istvan <dukusi@gmail.com>
2016-10-14 18:52:25 +0300

    Two flow rate sensor

--- Ulepito/Ulepito.ino
+++ Ulepito/Ulepito.ino
@@ -14,31 +14,83 @@ DATA_STRUCTURE rxdata;
 DATA_STRUCTURE txdata;
 SoftEasyTransfer ETin, ETout;
 
-SoftwareSerial mySerial(9, 8);
+SoftwareSerial mySerial(7, 8);
 //-------PINS--------------
 const int relayPin = 6;
+const int flowSensorPin1 = 2;
+const int flowSensorPin2 = 3;
 //-------------------------
+volatile long flowRpm1;
+volatile long flowRpm2;
+
+volatile long oldFlowRpm1;
+volatile long oldFlowRpm2;
+
+long lastMillis;
+
+void rpm1()     //This is the function that the interupt calls 
+{
+	flowRpm1++;  //This function measures the rising and falling edge of the 	hall effect sensors signal
+}
+
+void rpm2()     //This is the function that the interupt calls 
+{
+	flowRpm2++;  //This function measures the rising and falling edge of the 	hall effect sensors signal
+}
+
+void pumpOn()
+{
+	digitalWrite(relayPin, LOW);
+	Send("status/pump", "ON");
+}
+
+void pumpOff()
+{
+	digitalWrite(relayPin, HIGH);
+	Send("status/pump", "OFF");
+}
 
 void setup()
 {
 	Serial.begin(9600);
 	mySerial.begin(9600);
-	pinMode(relayPin, OUTPUT);
-	digitalWrite(relayPin, HIGH);
-
 	//start the library, pass in the data details and the name of the serial port.
 	ETin.begin(details(rxdata), &mySerial);
 	ETout.begin(details(txdata), &mySerial);
+
+	pinMode(relayPin, OUTPUT);
+	pumpOff();
+	attachInterrupt(digitalPinToInterrupt(flowSensorPin1), rpm1, RISING);
+	attachInterrupt(digitalPinToInterrupt(flowSensorPin2), rpm2, RISING);
+
     // delay startup until esp started
 	Send("Status", "Started");
+	sei();
 }
 
 void loop()
 {
-	//if (mySerial.available()) {
-	//	Serial.write(mySerial.read());
-	//}
-	
+	if (oldFlowRpm1 != flowRpm1)
+	{
+		Send("status/flow1", flowRpm1);
+		oldFlowRpm1 = flowRpm1;
+	}
+
+	if (oldFlowRpm2 != flowRpm2)
+	{
+		Send("status/flow2", flowRpm2);
+		oldFlowRpm2 = flowRpm2;
+	}
+
+	if (millis() - lastMillis > 45000 || millis()<lastMillis)
+	{
+		Serial.println(flowRpm1);
+		Serial.println(flowRpm2);
+		lastMillis = millis();
+		Send("status/flow1", flowRpm1);
+		Send("status/flow2", flowRpm2);
+	}
+
 	if (ETin.receiveData())
 	{
 		//message_buff[0] = 0;
@@ -53,14 +105,13 @@ void loop()
 			Serial.println("changing");
 			if (rxdata.payload[0] == '1')
 			{
-				digitalWrite(relayPin, LOW);
+				pumpOn();
 			}
 			else
 			{
-				digitalWrite(relayPin, HIGH);
+				pumpOff();
 			}
 		}
-
 	}
 
   /* add main program code here */
@@ -72,6 +123,12 @@ void Send(char* topic, char* payload)
 	txdata.length = strlen(payload);
 	strcpy(txdata.topic, topic);
 	memcpy(txdata.payload, payload, txdata.length+1);
-	Serial.println(txdata.topic);
 	ETout.sendData();
+}
+
+void Send(char* topic, int payload)
+{
+	char intStr[3];
+	itoa(payload, intStr, 10);
+	Send(topic, intStr);
 }
