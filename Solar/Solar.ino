2b34b323c7ea73dd0b7d881fdd76cc603cbfec39
Duka Istvan <dukusi@gmail.com>
2017-02-03 18:07:59 +0200

    Fixed two way communication

--- Solar/Solar.ino
+++ Solar/Solar.ino
@@ -9,6 +9,20 @@
 #define VOLTPIN A7 
 #define AMPPIN A6 
 #define INVOLTPIN A0 
+#define BUTTONPIN 11
+#define RELAY1PIN 2
+#define RELAY2PIN 3
+
+byte wifiChar[8] = {
+	0b00001,
+	0b00000,
+	0b00011,
+	0b00000,
+	0b00111,
+	0b00000,
+	0b01111,
+	0b11111
+};
 
 struct DATA_STRUCTURE {
 	char topic[100];
@@ -22,16 +36,12 @@ DATA_STRUCTURE rxdata;
 DATA_STRUCTURE txdata;
 SoftEasyTransfer ETin, ETout;
 
-SoftwareSerial mySerial(7, 8);
+SoftwareSerial mySerial(9, 8);
 
 DHT dht(DHTPIN, DHTTYPE);
 Adafruit_BMP085 bmp;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//-------PINS--------------
-const int relayPin = 6;
-const int ledPin = 9;
-//-------------------------
 
 int oldLevel1Status;
 int oldLevel2Status;
@@ -39,24 +49,39 @@ int oldLevel2Status;
 long lastMillis;
 long lastPingMillis;
 
-
-void pumpOn()
+void relay1On()
 {
-	digitalWrite(relayPin, LOW);
-	Send("status/pump", "ON");
+	digitalWrite(RELAY1PIN, LOW);
+	Send("status/relay1", "ON");
 }
 
-void pumpOff()
+void relay1Off()
 {
-	digitalWrite(relayPin, HIGH);
-	Send("status/pump", "OFF");
+	digitalWrite(RELAY1PIN, HIGH);
+	Send("status/relay1", "OFF");
+}
+
+void relay2On()
+{
+	digitalWrite(RELAY2PIN, LOW);
+	Send("status/relay2", "ON");
+}
+
+
+void relay2Off()
+{
+	digitalWrite(RELAY2PIN, HIGH);
+	Send("status/relay2", "OFF");
 }
 
 void setup()
 {
-	pinMode(relayPin, OUTPUT);
-	pinMode(ledPin, OUTPUT);
-	digitalWrite(ledPin, HIGH);
+
+	pinMode(RELAY1PIN, OUTPUT);
+	pinMode(RELAY2PIN, OUTPUT);
+
+
+	pinMode(BUTTONPIN, INPUT);
 
 	Serial.begin(9600);
 	mySerial.begin(9600);
@@ -64,8 +89,9 @@ void setup()
 	ETin.begin(details(rxdata), &mySerial);
 	ETout.begin(details(txdata), &mySerial);
 
+	relay1On();
+	relay2Off();
 
-	pumpOff();
 	dht.begin();
 
 	if (!bmp.begin()) {
@@ -74,6 +100,7 @@ void setup()
 
 	lcd.init();
 	lcd.backlight();
+	lcd.createChar(0, wifiChar);
 	lcd.clear();
 	lcd.print("Started");
 	// delay startup until esp started
@@ -85,43 +112,65 @@ void loop()
 {
 	if (ETin.receiveData())
 	{
-		Serial.println(rxdata.topic);
-		Serial.println((char*)rxdata.payload);
-		Serial.println(rxdata.length);
+		//Serial.println(rxdata.topic);
+		//Serial.println((char*)rxdata.payload);
+		//Serial.println(rxdata.length);
 		if (strcmp(rxdata.topic, "start") == 0)
 		{
 			SendAll();
 		}
-		//if (strcmp(rxdata.topic, "pump") == 0) {
-		//	Serial.println("changing");
-		//	if (rxdata.payload[0] == '1')
-		//	{
-		//		pumpOn();
-		//	}
-		//	else
-		//	{
-		//		pumpOff();
-		//	}
-		//}
+		if (strcmp(rxdata.topic, "relay1") == 0) {
+			Serial.println("changing");
+			if (rxdata.payload[0] == '1')
+			{
+				relay1On();
+				Serial.println("relay1 ON");
+			}
+			else
+			{
+				relay1Off();
+				Serial.println("relay1 OFF");
+			}
+		}
+		if (strcmp(rxdata.topic, "relay2") == 0) {
+			Serial.println("changing");
+			if (rxdata.payload[0] == '1')
+			{
+				relay2On();
+			}
+			else
+			{
+				relay2Off();
+			}
+		}
 	}
+
 	if (millis() - lastPingMillis > 1000 || millis() < lastPingMillis)
 	{
 
 		lastPingMillis = millis();
 	}
 
-	if (millis() - lastMillis > 5000 || millis() < lastMillis)
+	if (millis() - lastMillis > 15000 || millis() < lastMillis)
 	{
 		SendAll();
 		lastMillis = millis();
 	}
-
+	if (digitalRead(BUTTONPIN) == HIGH)
+	{
+		lcd.backlight();
+	}
+	else
+	{
+		lcd.noBacklight();
+	}
 	/* add main program code here */
 
 }
 
 void SendAll()
 {
+	lcd.setCursor(0, 0);
 	float t = dht.readTemperature();
 	float h = dht.readHumidity();
 	if (isnan(h) || isnan(t)) {
@@ -135,10 +184,13 @@ void SendAll()
 		Serial.print(" %\t");
 		Serial.print("Temperature: ");
 		Serial.print(t);
+		lcd.print((int)t);
+		lcd.print("C ");
 		Send("status/temperature0", t);
 		Serial.println(" *C ");
-
 	}
+
+
 
 	Serial.print("Temperature = ");
 	Serial.print(bmp.readTemperature());
@@ -150,27 +202,31 @@ void SendAll()
 	Serial.println(" Pa");
 	Send("status/pressure", (int)bmp.readPressure());
 
-	Serial.print("Pressure at sealevel (calculated) = ");
-	Serial.print((int)bmp.readSealevelPressure());
-	Serial.println(" Pa");
+	//Serial.print("Pressure at sealevel (calculated) = ");
+	//Serial.print((int)bmp.readSealevelPressure());
+	//Serial.println(" Pa");
 
 	Send("status/inttemp", (int)GetTemp());
 
-	int sensorValue = getVPP(VOLTPIN);
-	// Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-	float voltage = sensorValue *(5.0/1024.0) *5;
-	// print out the value you read:
-	Serial.print("Voltage ");
-	Serial.println(voltage);
-	Send("status/voltage", voltage);
+	//int sensorValue = getVPP(VOLTPIN);
+	//// Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+	//float voltage = sensorValue *(5.0/1024.0) *5;
+	//// print out the value you read:
+	//Serial.print("Voltage ");
+	//Serial.println(voltage);
+	//Send("status/voltage", voltage);
 
-	sensorValue = getVPP(INVOLTPIN);
+	int sensorValue = getVPP(INVOLTPIN);
 	// Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-	voltage = sensorValue *(5.0 / 1024.0) * 5;
+	float voltage = sensorValue *(5.0 / 1024.0) * 5;
 	// print out the value you read:
 	Serial.print("In Voltage ");
 	Serial.println(voltage);
 	Send("status/batteryvoltage", voltage);
+
+	lcd.print(voltage);
+	lcd.print("V ");
+
 	
 	sensorValue = getVPP(AMPPIN);
 	// Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
@@ -179,17 +235,19 @@ void SendAll()
 	Serial.print("Amper ");
 	Serial.println(amp);
 	Send("status/amper", amp);
+	lcd.print(amp);
+	lcd.print("A ");
 
 }
 
 void Send(char* topic, char* payload)
 {
-	digitalWrite(ledPin, HIGH);
+	//digitalWrite(ledPin, HIGH);
 	txdata.length = strlen(payload);
 	strcpy(txdata.topic, topic);
 	memcpy(txdata.payload, payload, txdata.length + 1);
 	ETout.sendData();
-	digitalWrite(ledPin, LOW);
+	//digitalWrite(ledPin, LOW);
 }
 
 void Send(char* topic, int payload)
@@ -211,12 +269,12 @@ float getVPP(int sensorIn)
 	unsigned int x = 0;
 	float AcsValue = 0.0, Samples = 0.0, AvgAcs = 0.0, AcsValueF = 0.0;
 
-	for (int x = 0; x < 150; x++) { //Get 150 samples
+	for (int x = 0; x < 25; x++) { //Get 150 samples
 		AcsValue = analogRead(sensorIn);     //Read current sensor values   
 		Samples = Samples + AcsValue;  //Add samples together
-		delay(3); // let ADC settle before next sample 3ms
+		delay(1); // let ADC settle before next sample 3ms
 	}
-	AvgAcs = Samples / 150.0;//Taking Average of Samples
+	AvgAcs = Samples / 25.0;//Taking Average of Samples
 	return AvgAcs;
 }
 
